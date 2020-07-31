98f6055c792f0d33e4283e8fa01a19cbf72fb185
Joshua Moyers <jmoyers@gmail.com>
2020-07-30 22:56:20 -0700

    Work in progress bring-up electron
    * We're at the point where we need the electron libraries
      so we need to be on the target platform
    * In this case thats windows, but not windows with wsl,
      since the build tools get confused and do whacky things

--- include/logs.hpp
+++ include/logs.hpp
@@ -9,6 +9,7 @@ using namespace std;
 namespace fs = std::filesystem;
 
 // enumerate all the files in a log directory
+// Q: is it better to use an iterator to be c++ idiomatic?
 vector<fs::path> find_files(const string directory) {
   vector<fs::path> files;
   for (auto const &f : fs::directory_iterator(directory)) {
@@ -17,6 +18,8 @@ vector<fs::path> find_files(const string directory) {
   return files;
 }
 
+// this is a function instead of a lambda because debugger was not working with
+// lambda, big sad
 bool is_eq_log(fs::path path) {
   return "eqlog" != path.filename().string().substr(0, 5);
 }
@@ -27,6 +30,9 @@ void filter_logs(vector<fs::path> &files) {
 }
 
 // load a log into memory from a file, all at once
+// this method seems to avoid extra copies and uses new c++17 feature to
+// retrieve the file size in a platform independent way
+// Q: is string return created like this RVO'd?
 string read_log(fs::path path) {
   ifstream f(path, ios::in | ios::binary);
 
